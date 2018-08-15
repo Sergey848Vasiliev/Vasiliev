6e7ef2c7deb842578b6ee10cf04a946c13cca5e2
vasilievsergey848 <foxinbox848@mail.com>
2018-08-15 21:19:23 +0300

    add fashionbride fix-1

--- demo/fashionbride/js/main.js
+++ demo/fashionbride/js/main.js
@@ -3,46 +3,45 @@
 
         $('.carousel').carousel({
             interval: 5000
-        })
-
-       /* $('.header-slider').slick({
-            infinite: true,
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            autoplay: true,
-            autoplaySpeed: 4000,
-            slide: "div",
-            dots: true,
-            arrows: true,
-            prevArrow: ".arr-left",
-            nextArrow: ".arr-right"
         });
 
-        $('.post-slider').slick({
-            infinite: true,
-            slidesToShow: 3,
-            slidesToScroll: 1,
-            autoplay: true,
-            autoplaySpeed: 4000,
-            slide: "div",
-            dots: false,
-            arrows: false
-            //prevArrow: ".arr-left",
-            //nextArrow: ".arr-right"
-        });*/
+        /* $('.header-slider').slick({
+             infinite: true,
+             slidesToShow: 1,
+             slidesToScroll: 1,
+             autoplay: true,
+             autoplaySpeed: 4000,
+             slide: "div",
+             dots: true,
+             arrows: true,
+             prevArrow: ".arr-left",
+             nextArrow: ".arr-right"
+         });
+
+         $('.post-slider').slick({
+             infinite: true,
+             slidesToShow: 3,
+             slidesToScroll: 1,
+             autoplay: true,
+             autoplaySpeed: 4000,
+             slide: "div",
+             dots: false,
+             arrows: false
+             //prevArrow: ".arr-left",
+             //nextArrow: ".arr-right"
+         });*/
 
         /*-----------плавный скролл----------------*/
-       /* $("#btn-card, #btn-about-us, #btn-help").on("click", function(e){  //на что кликаем
-            var anchor = $(this);
+        $("#btn-wedding-dresses, #btn-evening-dresses, #btn-wedding-veils, #btn-belt, #btn-decorations").on("click", function (e) {
+            let anchor = $(this);
             $('html, body').stop().animate({
                 scrollTop: $(anchor.attr('href')).offset().top
             }, 777);
             e.preventDefault();
-            return false;
-        });*/
+        });
 
         /*-----------стрелка вверх---------------*/
-       /* $(window).scroll(function(){
+        $(window).scroll(function () {
             if ($(this).scrollTop() > 100) {
                 $('.scroll-up').fadeIn();
             } else {
@@ -50,11 +49,10 @@
             }
         });
 
-        $('.scroll-up').click(function(){
-            $("html, body").animate({ scrollTop: 0 }, 600);
+        $('.scroll-up').click(function () {
+            $("html, body").animate({scrollTop: 0}, 600);
             return false;
-        });*/
-
+        });
     });
 })(jQuery);
 
